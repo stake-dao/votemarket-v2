f4b3919e9b74f6d09abb41bb4b2c72192335026c
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-07-17 17:44:26 +0200

    feat: IPG20 definition

---
+++ packages/bridge/src/interfaces/IPG20.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+/// @notice Interface for the PG20 token.
+/// @dev To be implemented in order to be able to be transported accross chains.
+interface IPG20 {}
