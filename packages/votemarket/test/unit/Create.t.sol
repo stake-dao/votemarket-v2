4856bea1fd8dadd4708f746b90f9b8cfe4c366ec
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-07-30 15:41:52 +0200

    feat: split in actions

--- packages/votemarket/test/unit/Create.t.sol
+++ packages/votemarket/test/unit/Create.t.sol
@@ -5,29 +5,13 @@ import "forge-std/src/Test.sol";
 import "forge-std/src/mocks/MockERC20.sol";
 
 import "src/Votemarket.sol";
+
 import "test/mocks/Hooks.sol";
+import "test/unit/Base.t.sol";
 
-contract VotemarketUnitTest is Test {
-    address creator = address(this);
-    MockERC20 rewardToken;
-    Votemarket votemarket;
-
-    // Test variables
-    address HOOK;
-    address[] blacklist;
-    uint256 constant CHAIN_ID = 1;
-    uint8 constant VALID_PERIODS = 4;
-    address constant GAUGE = address(0x1234);
-    address constant MANAGER = address(0x5678);
-    uint256 constant MAX_REWARD_PER_VOTE = 100e18;
-    uint256 constant TOTAL_REWARD_AMOUNT = 1000e18;
-
-    function setUp() public {
-        votemarket = new Votemarket();
-        rewardToken = new MockERC20();
-        rewardToken.initialize("Mock Token", "MOCK", 18);
-
-        HOOK = address(new MockHook());
+contract CreateCampaignTest is BaseTest {
+    function setUp() public override {
+        BaseTest.setUp();
     }
 
     function testCreateCampaign() public {
