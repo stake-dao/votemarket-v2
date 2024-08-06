d3408405d5bc6a4131564a3bb959f228f70e7b75
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-08-06 10:28:53 +0200

    feat: oracle lens impl wip

--- packages/votemarket/src/oracle/OracleLens.sol
+++ packages/votemarket/src/oracle/OracleLens.sol
@@ -12,7 +12,24 @@ contract OracleLens {
         oracle = _oracle;
     }
 
-    function getTotalVotes(address gauge, uint256 epoch) external view returns (uint256) {}
-    function canClaim(address account, address gauge, uint256 epoch) external view returns (bool) {}
-    function getAccountVotes(address account, address gauge, uint256 epoch) external view returns (uint256) {}
+    function getTotalVotes(address gauge, uint256 epoch) external view returns (uint256) {
+        IOracle.Point memory weight = IOracle(oracle).pointByEpoch(gauge, epoch);
+
+        return weight.bias;
+    }
+
+    function canClaim(address account, address gauge, uint256 epoch) external view returns (bool) {
+        IOracle.VotedSlope memory account_ = IOracle(oracle).votedSlopeByEpoch(account, gauge, epoch);
+        if (account_.slope == 0 || epoch >= account_.end || epoch <= account_.lastVote) return false;
+
+        return true;
+    }
+
+    function getAccountVotes(address account, address gauge, uint256 epoch) external view returns (uint256) {
+        IOracle.VotedSlope memory account_ = IOracle(oracle).votedSlopeByEpoch(account, gauge, epoch);
+
+        if (epoch >= account_.end) return 0;
+
+        return account_.slope * (account_.end - epoch);
+    }
 }
