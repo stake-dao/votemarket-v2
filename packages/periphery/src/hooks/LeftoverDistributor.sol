3f0cb0cc206f924b9a6806045b7568d144e1b5e5
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2025-05-02 14:42:02 +0200

    feat(packages/periphery) : Change Ownable for a Ownable 2 steps logic

--- packages/periphery/src/hooks/LeftoverDistributor.sol
+++ packages/periphery/src/hooks/LeftoverDistributor.sol
@@ -3,16 +3,24 @@ pragma solidity 0.8.19;
 
 import "@votemarket/src/interfaces/IVotemarket.sol";
 import "@solady/src/utils/SafeTransferLib.sol";
-import "@solady/src/auth/Ownable.sol";
 
 /// @notice Votemarket hook for receiving leftovers
 /// @custom:contact contact@stakedao.org
-contract NoRolloverHook is Ownable {
+contract LeftoverDistributor {
+    /// @notice Governance address.
+    address public governance;
+
+    /// @notice Future governance address.
+    address public futureGovernance;
+
     /// @notice Votemarket whitelist
     mapping(address => bool) public votemarkets;
 
     /// @notice Leftover recipient by campaign id by votemarket
     mapping(address => mapping(uint256 => address)) public leftoverRecipients;
+
+    /// @notice Thrown when a non-governance address attempts a governance-only action.
+    error AUTH_GOVERNANCE_ONLY();
 
     /// @notice Error thrown when an address shouldn't be zero
     error ZERO_ADDRESS();
@@ -35,13 +43,21 @@ contract NoRolloverHook is Ownable {
     /// @notice Emitted when a leftoverRecipients is set
     event LeftOverRecipientSet(address indexed votemarket, uint256 indexed campaignId, address indexed recipient);
 
+    /// @notice Emitted when a votemarket is enabled
+    event EnabledVotemarket(address indexed votemarket);
     /// @notice Emitted when a votemarket is toggled
-    event ToggleVotemarket(address indexed votemarket, bool enabled);
+    event DisabledVotemarket(address indexed votemarket);
 
-    /// @notice Initializes the owner.
-    /// @param _owner The address that will become the initial owner.
-    constructor(address _owner) {
-        _initializeOwner(_owner);
+    /// @notice Ensures that only the governance address can call the function.
+    modifier onlyGovernance() {
+        if (msg.sender != governance) revert AUTH_GOVERNANCE_ONLY();
+        _;
+    }
+
+    /// @notice Initializes the governance.
+    /// @param _governance The address that will become the initial governance.
+    constructor(address _governance) {
+        governance = _governance;
     }
 
     /// @notice Function called by votemarket during an _updateRewardPerVote when hook address is set
@@ -86,11 +102,18 @@ contract NoRolloverHook is Ownable {
         emit LeftOverRecipientSet(_votemarket, _campaignId, _recipient);
     }
 
-    /// @notice A function to toggle usable votemarkets on this contract
+    /// @notice A function to enable votemarkets on this contract
     /// @param _votemarket Votemarket address to toggle
-    function toggleVotemarket(address _votemarket) external onlyOwner {
-        votemarkets[_votemarket] = !votemarkets[_votemarket];
-        emit ToggleVotemarket(_votemarket, votemarkets[_votemarket]);
+    function enableVotemarket(address _votemarket) external onlyGovernance {
+        votemarkets[_votemarket] = true;
+        emit EnabledVotemarket(_votemarket);
+    }
+
+    /// @notice A function to disable votemarkets on this contract
+    /// @param _votemarket Votemarket address to toggle
+    function disableVotemarket(address _votemarket) external onlyGovernance {
+        votemarkets[_votemarket] = false;
+        emit DisabledVotemarket(_votemarket);
     }
 
     /// @notice A function that rescue any ERC20 token
@@ -98,8 +121,22 @@ contract NoRolloverHook is Ownable {
     /// @param _token token address
     /// @param _amount amount to rescue
     /// @param _recipient address to send token rescued
-    function rescueERC20(address _token, uint256 _amount, address _recipient) external onlyOwner {
+    function rescueERC20(address _token, uint256 _amount, address _recipient) external onlyGovernance {
         if (_recipient == address(0)) revert ZERO_ADDRESS();
         SafeTransferLib.safeTransfer(_token, _recipient, _amount);
+    }
+
+    /// @notice Sets the future governance address
+    /// @param _futureGovernance The new future governance address
+    function transferGovernance(address _futureGovernance) external onlyGovernance {
+        if (_futureGovernance == address(0)) revert ZERO_ADDRESS();
+        futureGovernance = _futureGovernance;
+    }
+
+    /// @notice Accepts the governance role via the future governance address
+    function acceptGovernance() external {
+        if (msg.sender != futureGovernance) revert AUTH_GOVERNANCE_ONLY();
+        governance = futureGovernance;
+        futureGovernance = address(0);
     }
 }
--- packages/periphery/test/hooks/LeftoverDistributor.t.sol
+++ packages/periphery/test/hooks/LeftoverDistributor.t.sol
@@ -4,12 +4,12 @@ pragma solidity 0.8.19;
 import "@forge-std/src/Test.sol";
 import {IVotemarket, Campaign} from "@votemarket/src/interfaces/IVotemarket.sol";
 import {FakeToken} from "../mocks/FakeToken.sol";
-import {NoRolloverHook} from "../../src/hooks/NoRolloverHook.sol";
+import {LeftoverDistributor} from "../../src/hooks/LeftoverDistributor.sol";
 
-contract NoRolloverHookTest is Test {
+contract LeftoverDistributorTest is Test {
     FakeToken rewardToken;
     IVotemarket votemarket = IVotemarket(0x8c2c5A295450DDFf4CB360cA73FCCC12243D14D9);
-    NoRolloverHook hook;
+    LeftoverDistributor hook;
     uint256 campaignId;
     uint256 expectedLeftOver = 499992810414009245950;
 
@@ -25,7 +25,7 @@ contract NoRolloverHookTest is Test {
         rewardToken = new FakeToken("Mock Token", "MOCK", 18);
         rewardToken.mint(address(this), 1000e18);
 
-        hook = new NoRolloverHook(address(this));
+        hook = new LeftoverDistributor(address(this));
 
         rewardToken.approve(address(votemarket), 1000e18);
         // sdCRV gauge, which has votes
@@ -60,37 +60,42 @@ contract NoRolloverHookTest is Test {
     // Test errors on a votemarket platform not set
     function test_unsetVotemarketExpectedErrors() public {
         // calling doSomething if not whitelisted in the hook
-        vm.expectRevert(NoRolloverHook.UNAUTHORIZED_VOTEMARKET.selector);
+        vm.expectRevert(LeftoverDistributor.UNAUTHORIZED_VOTEMARKET.selector);
         hook.doSomething(campaignId, 0, address(rewardToken), 0, 1 gwei, bytes("0x"));
 
         // calling setRecipient on a not set votemarket
-        vm.expectRevert(NoRolloverHook.UNAUTHORIZED_VOTEMARKET.selector);
+        vm.expectRevert(LeftoverDistributor.UNAUTHORIZED_VOTEMARKET.selector);
         hook.setLeftOverRecipient(address(votemarket), campaignId, alice);
     }
 
-    /// Tests to toggle votemarket addresses
+    /// Tests to enable / disable votemarket addresses
     function test_toggleVotemarket() public {
         // Expect to revert if not owner
         vm.prank(alice);
         vm.expectRevert();
-        hook.toggleVotemarket(address(votemarket));
+        hook.enableVotemarket(address(votemarket));
 
         // Expect to be false by default
         assertEq(hook.votemarkets(address(votemarket)), false);
-        hook.toggleVotemarket(address(votemarket));
+        hook.enableVotemarket(address(votemarket));
         assertEq(hook.votemarkets(address(votemarket)), true);
 
-        // Expect to unset already set
-        hook.toggleVotemarket(address(votemarket));
+        // Expect to revert if not owner
+        vm.prank(alice);
+        vm.expectRevert();
+        hook.disableVotemarket(address(votemarket));
+
+        // Expect to unset
+        hook.disableVotemarket(address(votemarket));
         assertEq(hook.votemarkets(address(votemarket)), false);
     }
 
     /// Tests for recipient settings
     function test_setLeftoverRecipient() public {
-        hook.toggleVotemarket(address(votemarket));
+        hook.enableVotemarket(address(votemarket));
 
         // Not anyone can set the recipient
-        vm.expectRevert(NoRolloverHook.UNAUTHORIZED.selector);
+        vm.expectRevert(LeftoverDistributor.UNAUTHORIZED.selector);
         hook.setLeftOverRecipient(address(votemarket), campaignId, address(this));
 
         // Alice is the manager, can set the recipient
@@ -103,17 +108,17 @@ contract NoRolloverHookTest is Test {
         assertEq(hook.leftoverRecipients(address(votemarket), campaignId), bob);
 
         // Old recipient can't set an other recipient
-        vm.expectRevert(NoRolloverHook.UNAUTHORIZED.selector);
+        vm.expectRevert(LeftoverDistributor.UNAUTHORIZED.selector);
         hook.setLeftOverRecipient(address(votemarket), campaignId, address(this));
 
         // Can't set recipient for not existing campaign
-        vm.expectRevert(NoRolloverHook.UNAUTHORIZED.selector);
+        vm.expectRevert(LeftoverDistributor.UNAUTHORIZED.selector);
         hook.setLeftOverRecipient(address(votemarket), campaignId + 1, address(this));
     }
 
     /// Test default flow, leftover is sent to the manager
     function test_defaultFlowWithVotemarket() public {
-        hook.toggleVotemarket(address(votemarket));
+        hook.enableVotemarket(address(votemarket));
 
         votemarket.updateEpoch(campaignId, block.timestamp / 1 weeks * 1 weeks, bytes("0x"));
 
@@ -123,7 +128,7 @@ contract NoRolloverHookTest is Test {
 
     /// Test if recipient set does send the leftover to the right address
     function test_recipientFlowWithVotemarket() public {
-        hook.toggleVotemarket(address(votemarket));
+        hook.enableVotemarket(address(votemarket));
 
         vm.prank(alice);
         hook.setLeftOverRecipient(address(votemarket), campaignId, bob);
@@ -138,7 +143,7 @@ contract NoRolloverHookTest is Test {
 
     /// Test if the changing the manager also changes the default recipient
     function test_defaultFlowChangingManagerOnVotemarket() public {
-        hook.toggleVotemarket(address(votemarket));
+        hook.enableVotemarket(address(votemarket));
 
         vm.prank(alice);
         votemarket.updateManager(campaignId, bob);
@@ -153,7 +158,7 @@ contract NoRolloverHookTest is Test {
     /// /!\ /!\ /!\  TO AVOID  /!\ /!\ /!\
     /// Flow in case a votemarket platform is not correctly toggled in the contract
     function test_unsetVotemarketIssue() public {
-        // We don't toggle the platform on purpose to revert the doSomething call
+        // We don't enable the platform on purpose to revert the doSomething call
 
         votemarket.updateEpoch(campaignId, block.timestamp / 1 weeks * 1 weeks, bytes("0x"));
 
